000593ac6ddfbabfa001b8a0c937cd4eadcc3548
DEBADRIBASAK <32904247+DEBADRIBASAK@users.noreply.github.com>
2018-04-08 16:50:18 +0530

    Create admin.py

---
+++ makeathon/Libr/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Student,Book,Issue
+# Register your models here.
+
+admin.site.register(Student)
+admin.site.register(Book)
+admin.site.register(Issue)
